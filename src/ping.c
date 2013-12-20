127c73ec21001427792f13736d89a8c15063dd2b
Clément Schreiner <clement@mux.me>
2013-12-20 12:21:41 +0100

    Debut correction.

--- src/ping.c
+++ src/ping.c
@@ -36,7 +36,10 @@ int main(int argc, char *argv[]) {
     }
 
     if((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1) {
-        perror("impossible d'ouvrir la socket ");
+        if (errno == EPERM)
+            debug("Le programme doit être lancé en root.\n");
+        else
+            perror("Impossible d'ouvrir la socket ");
         close(sockfd);
         exit(EXIT_FAILURE);
     }
@@ -83,16 +86,16 @@ int main(int argc, char *argv[]) {
            pkt_r.icmp.seq_num == pkt_s->seq_num)
         {
             time = (1000000 - (long int) interval.tv_usec) / 1000;
-            if (TIMEOUT-1-interval.tv_sec > 0)
+            if (TIMEOUT - 1 - interval.tv_sec > 0)
             {
-                printf("%d time =  %ld sec et %ld ms\n", i+1,
-                       (long int)TIMEOUT-1-interval.tv_sec, time);
-                mean += 1000 * (long int)TIMEOUT-1-interval.tv_sec;
+                printf("%d time =  %ld sec et %ld ms\n", i + 1,
+                       (long int)TIMEOUT - 1 - interval.tv_sec, time);
+                mean += 1000 * (long int)TIMEOUT - 1-interval.tv_sec;
                 mean += time;
             }
             else
             {
-                printf("%d time = %ld ms\n", i+1, time);
+                printf("%d time = %ld ms\n", i + 1, time);
                 mean += time;
             }
         }
