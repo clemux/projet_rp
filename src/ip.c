fe62c2f80dfbd85e664327169eac8f929f7fa9ee
Clément Schreiner <clement@mux.me>
2013-12-20 06:27:23 +0100

    Oublie checksum.

---
+++ src/ip.c
@@ -0,0 +1,21 @@
+#include "ip.h"
+
+uint16_t checksum(uint16_t *buffer, uint32_t size) {
+    unsigned long sum = 0;
+    
+    while(size > 1) 
+    {
+        sum += *buffer++;
+        size -= sizeof(uint16_t);
+    }
+    
+    if(size == 1) 
+    {
+        sum += *(uint8_t*)buffer;
+    }
+    
+    sum = (sum >> 16) + (sum & 0xffff);
+    sum += (sum >>16);
+    
+    return (uint16_t)(~sum); /* on retourne le complément à 1 de la somme */
+}
